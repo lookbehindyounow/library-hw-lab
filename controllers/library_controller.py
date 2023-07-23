efdfcf0a6c3e4439b7407404e8067b0620377b4f
Kieron Harrower <somebody.else.2017@gmail.com>
2023-07-23 10:20:14 +0100

    fixed handling of data from checked out buttons on form & now the submit button from the form takes you to the page of the book you just added

--- controllers/library_controller.py
+++ controllers/library_controller.py
@@ -15,15 +15,16 @@ def call_book(index):
 def form():
     return render_template('form.jinja',title='Add Book',books=books)
 
-@events_blueprint.route('/',methods=["POST"]) # change route to book page
-def add_book():
-    book=Book(request.form["title"],request.form["author"],request.form["genre"],request.form["checked"])
-    if request.form["title"] not in [book.title for book in books] and request.form["author"] not in [book.author for book in books]:
+@events_blueprint.route('/books/<index>',methods=["POST"]) # change route to book page
+def add_book(index):
+    book=Book(request.form["title"],request.form["author"],request.form["genre"],bool(request.form["checked"]))
+    if request.form["title"] not in [book.title for book in books] or request.form["author"] not in [book.author for book in books]:
         books.append(book)
-    return render_template('index.jinja',title="Library",books=books) # change route
+    print([books[i].checked_out for i in range(len(books)) if i==len(books)-1])
+    return render_template('book.jinja',title=books[int(index)].title,book=books[int(index)])
 
 @events_blueprint.route('/books/<index>/delete',methods=["POST"])
-def remove_event(index):
+def remove_book(index):
     print(index,len(books),[book.title for book in books])
     del books[int(index)]
     return redirect('/')
