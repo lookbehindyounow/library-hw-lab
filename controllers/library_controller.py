37f48add0fa707d64aceadc9917ed685e58d2bc5
Kieron Harrower <somebody.else.2017@gmail.com>
2023-07-23 10:33:49 +0100

    pre-selected available button on form so wouldn't get any no input errors (blank titles & authors are fine cause you never know what people might call their books), if you make a duplicate book it now takes you to an empty form instead of trying to take you to the page of the book you just didn't add

--- controllers/library_controller.py
+++ controllers/library_controller.py
@@ -15,13 +15,14 @@ def call_book(index):
 def form():
     return render_template('form.jinja',title='Add Book',books=books)
 
-@events_blueprint.route('/books/<index>',methods=["POST"]) # change route to book page
+@events_blueprint.route('/books/<index>',methods=["POST"])
 def add_book(index):
     book=Book(request.form["title"],request.form["author"],request.form["genre"],bool(request.form["checked"]))
+    # LOGIC NEEDS FIXED IN LINE BELOW
     if request.form["title"] not in [book.title for book in books] or request.form["author"] not in [book.author for book in books]:
         books.append(book)
-    print([books[i].checked_out for i in range(len(books)) if i==len(books)-1])
-    return render_template('book.jinja',title=books[int(index)].title,book=books[int(index)])
+        return render_template('book.jinja',title=books[int(index)].title,book=books[int(index)])
+    return redirect('/form')
 
 @events_blueprint.route('/books/<index>/delete',methods=["POST"])
 def remove_book(index):
